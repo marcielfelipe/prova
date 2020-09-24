943c1463d6c115483c7535c3c05842ea1869b0b2
Marciel Felipe <42660706+marcielfelipe@users.noreply.github.com>
2020-09-24 20:31:18 -0300

    primeira verificação

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const local4=[3]
 const cod=0
 
 
-function add(){
+function add(cotainer){
   const l1=local1.length
   const l2=local2.length
   const l3=local3.length
@@ -15,7 +15,34 @@ function add(){
 
   if(l1<l2 &&l1<l3&&l1,l4){
     if(l1<4){
-      
+      console.log('Impossível empilhar!')
+    }else{
+      local1.push(cotainer)
+      console.log('Adicionado ao local 1')
+    }
+  }
+  else if(l2<l1 &&l2<l3&&l2,l4){
+    if(l2<4){
+      console.log('Impossível empilhar!')
+    }else{
+      local2.push(cotainer)
+      console.log('Adicionado ao local 2')
+    }
+  }
+  else if(l3<l1 &&l3<l2&&l2,l4){
+    if(l3<4){
+      console.log('Impossível empilhar!')
+    }else{
+      local2.push(cotainer)
+      console.log('Adicionado ao local 3')
+    }
+  }
+  else if(l3<l1 &&l3<l2&&l3,l4){
+    if(l3<4){
+      console.log('Impossível empilhar!')
+    }else{
+      local2.push(cotainer)
+      console.log('Adicionado ao local 3')
     }
   }
 
