c0b78f6561877aadf41a11d4d736f9fa7d1ce3f7
Marciel Felipe <42660706+marcielfelipe@users.noreply.github.com>
2020-09-24 20:18:51 -0300

    first

---
+++ index.js
@@ -0,0 +1,28 @@
+
+const local1=[3]
+const local2=[3]
+const local3=[3]
+const local4=[3]
+
+const cod=0
+
+
+function add(){
+  const l1=local1.length
+  const l2=local2.length
+  const l3=local3.length
+  const l4=local4.length
+
+  if(l1<l2 &&l1<l3&&l1,l4){
+    if(l1<4){
+      
+    }
+  }
+
+}
+function rem(){
+  for (let index = 0; index < local1.length; index++) {
+    const element = local1[index];
+    
+  }
+}
